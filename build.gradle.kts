baece91ab7557055e362668cfff744a5a42ecc28
rakesh.chauhan <rakesh.chauhan@0822LTPB0076228.local>
2023-11-27 16:42:30 +0530

    Rakesh\Pranav| Adding dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("java")
+    id("io.spring.dependency-management") version "1.1.4"
+    id("org.springframework.boot") version "3.1.5"
+    id("org.flywaydb.flyway") version "9.22.3"
 }
 
 group = "org.example"
@@ -10,8 +13,14 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.h2database:h2:2.1.210")
+    implementation("org.flywaydb:flyway-core:9.22.3")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.assertj:assertj-core:3.24.2")
 }
 
 tasks.test {
