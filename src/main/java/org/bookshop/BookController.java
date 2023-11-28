f0825fda2b59e734767d1a8fdeba35ca7bf5a0b7
raveendra.pai <raveendra.pai@idfcbank.com>
2023-11-28 15:15:28 +0530

    Raveendra | Adding cross origin

--- src/main/java/org/bookshop/BookController.java
+++ src/main/java/org/bookshop/BookController.java
@@ -2,11 +2,13 @@ package org.bookshop;
 
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class BookController {
 
