3336951775b46f8e06449532839f1d4c2d58589d
Yash Agarwal <yashagar0412@gmail.com>
2023-12-01 15:33:34 +0530

    Yash/Raveendra: Housekeeping in Book controller

--- src/main/java/org/bookshop/book/BookController.java
+++ src/main/java/org/bookshop/book/BookController.java
@@ -3,7 +3,10 @@ package org.bookshop.book;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -19,15 +22,15 @@ public class BookController {
     }
 
     @GetMapping("books")
-    public ResponseEntity<Books> allBooks(@RequestParam(value ="query", required =false) String query){
-        if (query == null || query == "") {
+    public ResponseEntity<Books> allBooks(@RequestParam(value = "query", required = false) String query) {
+        if (query == null || query.isEmpty()) {
             List<Book> allBooks = bookService.getAllBooks();
             return ResponseEntity.ok(new Books(allBooks));
         }
-        if (query.length() <3){
+        if (query.length() < 3) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Enter Valid Request");
         }
-        List<Book> searchedBook =bookService.searchBook(query);
+        List<Book> searchedBook = bookService.searchBook(query);
         return ResponseEntity.ok(new Books(searchedBook));
     }
 
